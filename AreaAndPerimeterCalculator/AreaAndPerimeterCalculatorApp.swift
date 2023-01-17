bcbac5389e9592e3b412733e3051e545f1a83f5b
Justin Hui <jhui@lcs.on.ca>
2023-01-17 11:34:37 -0500

    Initial Commit

---
+++ AreaAndPerimeterCalculator/AreaAndPerimeterCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AreaAndPerimeterCalculatorApp.swift
+//  AreaAndPerimeterCalculator
+//
+//  Created by Justin Hui on 17/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct AreaAndPerimeterCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
